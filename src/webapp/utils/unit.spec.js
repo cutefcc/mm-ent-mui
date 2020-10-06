bd8392ce61559162ad41c2644a26302fe7ddd7c1
liuhao <liuhao@taou.com>
2020-10-06 21:08:14 +0800

    fix: 🐭 unit test

--- src/webapp/utils/unit.spec.js
+++ src/webapp/utils/unit.spec.js
@@ -3,6 +3,6 @@ describe('加法函数的测试', function () {
         expect(addOne(1)).toBe(2);
     });
     it("1加2 应该 === 3", function () {
-        expect(addTwo(1)).toBe(4);
+        expect(addTwo(1)).toBe(3);
     });
 });
